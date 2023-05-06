8d49986d93559fc900140402257c27d82d4e8ec1
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-05-06 15:50:29 -0400

    Refactor API v1 routes

--- src/api/v1/routes/movieRoutes.js
+++ src/api/v1/routes/movieRoutes.js
@@ -1,97 +1,57 @@
 const express = require("express");
+const movieController = require("../controllers/movieControllers");
 
 const router = express.Router();
 
 // Retrieves a list of all movies in the database
-router.get("/movies", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a list of all movies in the database.",
-    });
-});
+router.get("/movies", movieController.getAllMovies);
 
 // Retrieves a specific movie by its unique title_id
-router.get("/movie/id/:title_id", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a specific movie by its unique title_id.",
-    });
-});
+router.get("/movie/id/:title_id", movieController.getMovieByTitleId);
 
 // Retrieves the poster image for a specific movie
-router.get("/movie/:title_id/poster", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the poster image for a specific movie.",
-    });
-});
+router.get("/movie/:title_id/poster", movieController.getMoviePosterByTitleId);
 
 // Retrieves the release year for a specific movie
-router.get("/movie/:title_id/year", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the release year for a specific movie.",
-    });
-});
+router.get(
+    "/movie/:title_id/year",
+    movieController.getMovieReleaseYearByTitleId
+);
 
 // Retrieves the rating for a specific movie
-router.get("/movie/:title_id/rating", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the rating for a specific movie.",
-    });
-});
+router.get("/movie/:title_id/rating", movieController.getMovieRatingByTitleId);
 
 // Retrieves the certificate rating for a specific movie (e.g. PG-13, R, etc.)
-router.get("/movie/:title_id/certificate", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the certificate rating for a specific movie.",
-    });
-});
+router.get(
+    "/movie/:title_id/certificate",
+    movieController.getMovieCertificateByTitleId
+);
 
 // Retrieves the runtime (in minutes) for a specific movie
-router.get("/movie/:title_id/runtime", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the runtime (in minutes) for a specific movie.",
-    });
-});
+router.get(
+    "/movie/:title_id/runtime",
+    movieController.getMovieRuntimeByTitleId
+);
 
 // Retrieves the genre(s) for a specific movie
-router.get("/movie/:title_id/genre", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves the genre(s) for a specific movie.",
-    });
-});
+router.get("/movie/:title_id/genre", movieController.getMovieGenreByTitleId);
 
 // Retrieves a list of the top-rated movies in the database
-router.get("/movies/top_rated", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a list of the top-rated movies in the database.",
-    });
-});
+router.get("/movies/top_rated", movieController.getTopRatedMovies);
 
 // Retrieves a list of movies released in a specific year
-router.get("/movies/:year", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a list of movies released in a specific year.",
-    });
-});
+router.get("/movies/:year", movieController.getMoviesByYear);
 
 // Retrieves a list of movies with a specific certificate rating
-router.get("/movies/certificate/:certificate", (req, res) => {
-    res.status(200).send({
-        message:
-            "Retrieves a list of movies with a specific certificate rating.",
-    });
-});
+router.get(
+    "/movies/certificate/:certificate",
+    movieController.getMoviesByCertification
+);
 
 // Retrieves a list of movies with a specific genre
-router.get("/movies/genre/:genre", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a list of movies with a specific genre.",
-    });
-});
+router.get("/movies/genre/:genre", movieController.getMoviesByGenre);
 
 // Retrieves a random movie from the database
-router.get("/movie/random", (req, res) => {
-    res.status(200).send({
-        message: "Retrieves a random movie from the database.",
-    });
-});
+router.get("/movie/random", movieController.getRandomMovie);
 
 module.exports = router;

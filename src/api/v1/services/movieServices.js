45446db95e175d13fe8b79b3a942aaf4c36d6f96
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-05-07 12:59:26 -0400

    Add API v1 service methods

---
+++ src/api/v1/services/movieServices.js
@@ -0,0 +1,67 @@
+const getAllMovies = () => {
+  return;
+};
+
+const getMovieByTitleId = () => {
+  return;
+};
+
+const getMoviePosterByTitleId = () => {
+  return;
+};
+
+const getMovieReleaseYearByTitleId = () => {
+  return;
+};
+
+const getMovieRatingByTitleId = () => {
+  return;
+};
+
+const getMovieCertificateByTitleId = () => {
+  return;
+};
+
+const getMovieRuntimeByTitleId = () => {
+  return;
+};
+
+const getMovieGenreByTitleId = () => {
+  return;
+};
+
+const getTopRatedMovies = () => {
+  return;
+};
+
+const getMoviesByYear = () => {
+  return;
+};
+
+const getMoviesByCertification = () => {
+  return;
+};
+
+const getMoviesByGenre = () => {
+  return;
+};
+
+const getRandomMovie = () => {
+  return;
+};
+
+module.exports = {
+  getAllMovies,
+  getMovieByTitleId,
+  getMoviePosterByTitleId,
+  getMovieReleaseYearByTitleId,
+  getMovieRatingByTitleId,
+  getMovieCertificateByTitleId,
+  getMovieRuntimeByTitleId,
+  getMovieGenreByTitleId,
+  getTopRatedMovies,
+  getMoviesByYear,
+  getMoviesByCertification,
+  getMoviesByGenre,
+  getRandomMovie,
+};

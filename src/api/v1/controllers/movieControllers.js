0ec9705035a3cda8afeb665d6d18067b030793cd
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-05-07 15:10:16 -0400

    Add service methods in controller

--- src/api/v1/controllers/movieControllers.js
+++ src/api/v1/controllers/movieControllers.js
@@ -1,67 +1,82 @@
+const movieService = require("../services/movieServices");
+
 const getAllMovies = (req, res) => {
-    res.send("Get all movies.");
+  const allMovies = movieService.getAllMovies();
+  res.send("Get all movies.");
 };
 
 const getMovieByTitleId = (req, res) => {
-    res.send("Get movie by title_id.");
+  const movie = movieService.getMovieByTitleId();
+  res.send("Get movie by title_id.");
 };
 
 const getMoviePosterByTitleId = (req, res) => {
-    res.send("Get movie poster by title_id.");
+  const movie = movieService.getMoviePosterByTitleId();
+  res.send("Get movie poster by title_id.");
 };
 
 const getMovieReleaseYearByTitleId = (req, res) => {
-    res.send("Get movie release Year by title_id.");
+  const movie = movieService.getMovieReleaseYearByTitleId();
+  res.send("Get movie release Year by title_id.");
 };
 
 const getMovieRatingByTitleId = (req, res) => {
-    res.send("Get movie rating by title_id.");
+  const movie = movieService.getMovieRatingByTitleId();
+  res.send("Get movie rating by title_id.");
 };
 
 const getMovieCertificateByTitleId = (req, res) => {
-    res.send("Get movie certificate by title_id.");
+  const movie = movieService.getMovieCertificateByTitleId();
+  res.send("Get movie certificate by title_id.");
 };
 
 const getMovieRuntimeByTitleId = (req, res) => {
-    res.send("Get movie runtime by title_id.");
+  const movie = movieService.getMovieRuntimeByTitleId();
+  res.send("Get movie runtime by title_id.");
 };
 
 const getMovieGenreByTitleId = (req, res) => {
-    res.send("Get movie genre(s) by title_id.");
+  const movie = movieService.getMovieGenreByTitleId();
+  res.send("Get movie genre(s) by title_id.");
 };
 
 const getTopRatedMovies = (req, res) => {
-    res.send("Get a list of top-rated Movies.");
+  const movies = movieService.getTopRatedMovies();
+  res.send("Get a list of top-rated Movies.");
 };
 
 const getMoviesByYear = (req, res) => {
-    res.send("Get a list of movies released in a specific year.");
+  const movies = movieService.getMoviesByYear();
+  res.send("Get a list of movies released in a specific year.");
 };
 
 const getMoviesByCertification = (req, res) => {
-    res.send("Get a list of movies with a specific certification rating.");
+  const movies = movieService.getMoviesByCertification();
+  res.send("Get a list of movies with a specific certification rating.");
 };
 
 const getMoviesByGenre = (req, res) => {
-    res.send("Get a list of movies with a specific genre.");
+  const movies = movieService.getMoviesByGenre();
+  res.send("Get a list of movies with a specific genre.");
 };
 
 const getRandomMovie = (req, res) => {
-    res.send("Get a random movie.");
+  const movie = movieService.getRandomMovie();
+  res.send("Get a random movie.");
 };
 
 module.exports = {
-    getAllMovies,
-    getMovieByTitleId,
-    getMoviePosterByTitleId,
-    getMovieReleaseYearByTitleId,
-    getMovieRatingByTitleId,
-    getMovieCertificateByTitleId,
-    getMovieRuntimeByTitleId,
-    getMovieGenreByTitleId,
-    getTopRatedMovies,
-    getMoviesByYear,
-    getMoviesByCertification,
-    getMoviesByGenre,
-    getRandomMovie,
+  getAllMovies,
+  getMovieByTitleId,
+  getMoviePosterByTitleId,
+  getMovieReleaseYearByTitleId,
+  getMovieRatingByTitleId,
+  getMovieCertificateByTitleId,
+  getMovieRuntimeByTitleId,
+  getMovieGenreByTitleId,
+  getTopRatedMovies,
+  getMoviesByYear,
+  getMoviesByCertification,
+  getMoviesByGenre,
+  getRandomMovie,
 };

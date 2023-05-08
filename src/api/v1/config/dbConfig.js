f9a4305c8f1a283e979e6c066175c0d1265cd3e5
Ravi Kant Pujari <rvkantpujari@gmail.com>
2023-05-08 03:12:40 -0400

    Refactor mongoose connect promise

--- src/api/v1/config/dbConfig.js
+++ src/api/v1/config/dbConfig.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async (URI) => {
-  const conn = await mongoose.connect(URI);
-  return conn;
+    const conn = await mongoose
+        .connect(URI)
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((error) => console.log(error));
+    return conn;
 };
 
 module.exports = connectDB;
